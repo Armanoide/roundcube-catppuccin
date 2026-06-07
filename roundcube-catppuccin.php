369fead1e592d4f4274d2cdc375089b776965d6d
Norbert Billa <hello@armanoide.net>
2026-06-07 09:58:35 +0200

    feat: bootstrap plugin with composer and class skeleton

---
+++ roundcube-catppuccin.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Catppuccin Theme Loader Plugin for Roundcube
+ *
+ * @license MIT
+ * @author Armanoide
+ */
+class roundcube_catppuccin extends rcube_plugin
+{
+    public $task = '.*';
+
+    public function init()
+    {
+        $rcmail = rcmail::get_instance();
+
+        // We only target the official Elastic skin
+        if ($rcmail->config->get('skin') === 'elastic') {
+            // Read the preferred flavor from config, default to mocha
+            $flavor = $rcmail->config->get('catppuccin_flavor', 'mocha');
+
+            // Dynamically include the correct stylesheet from the plugin folder
+            $this->include_stylesheet("catppuccin-{$flavor}/styles.css");
+        }
+    }
+}
